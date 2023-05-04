16cc1d262572392508b9edc5017401fb569737d7
christianarca27 <christian.arca.mail@gmail.com>
2023-05-04 15:49:08 +0200

    feat: aggiunto prodotto di tipo "generico" all'array + stampa dei prodotti sotto forma di card (tramite bootstrap)

--- index.php
+++ index.php
@@ -6,18 +6,17 @@ require_once './Models/Food.php';
 require_once './Models/Toy.php';
 
 $categories = [
-    "dogs" => new Category("Cani"),
-    "cats" => new Category("Gatti")
+    "dogs" => new Category("Cani", "https://icon-library.com/images/dog-png-icon/dog-png-icon-6.jpg"),
+    "cats" => new Category("Gatti", "https://cdn.icon-icons.com/icons2/2248/PNG/512/cat_icon_138789.png")
 ];
 
 $products = [
+    new Product("https://arcaplanet.vtexassets.com/arquivos/ids/254499-300-300/litterlocker-refills-sacchetti-ricambio.jpg?v=1769606995&quality=1&width=300&height=300", "Sacchetti Ricambio", $categories["cats"], 18.69, "lorem"),
     new Food("https://arcaplanet.vtexassets.com/arquivos/ids/284156/pedigree-dentastix-medium-main.jpg?v=638174869086130000", "Dentastix", $categories["dogs"], 30.99, "lorem", "105 pcs", ["prodotti di origine vegetale", "carni varie"], "1pc al giorno"),
     new Toy("https://arcaplanet.vtexassets.com/arquivos/ids/272941/frisbee-rosso-15-cm.jpg?v=1769591173", "Frisbee", $categories["dogs"], 4.69, "lorem", "15 cm"),
     new Food("https://arcaplanet.vtexassets.com/arquivos/ids/207882/Multipack-Next-Natural-Gatto-Tonnetto-e-Salmone-in-gelatina-6x50g.jpg?v=1769602330", "Next Cat Natural Tonnetto Salmone", $categories["cats"], 6.99, "lorem", "6x50g", ["tonnetto", "salmone", "riso"], "fino a 2 lattine al giorno"),
     new Toy("https://arcaplanet.vtexassets.com/arquivos/ids/277674/ap20036.jpg?v=1768868507", "Tiragraffi", $categories["cats"], 54, "lorem", "61x45x105 cm")
 ];
-
-var_dump($products);
 
 ?>
 
@@ -29,12 +28,41 @@ var_dump($products);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP OOP 2</title>
+
+    <!-- style -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
 </head>
 
 <body>
-    <h1>PHP OOP 2</h1>
+    <div class="container">
+        <h1>PHP OOP 2</h1>
 
+        <ul id="product-list" class="card-group">
+            <?php
+            foreach ($products as $product) {
+            ?>
+                <li class="card product">
+                    <img class="card-img-top product-image" src="<?= $product->getImageUrl() ?>" alt="">
+                    <div class="card-body">
+                        <h2 class="card-title"><?= $product->getName() ?></h2>
+                        <span>Prezzo: € <?= $product->getPrice() ?></span>
+                        <div class="product-category">
+                            <span>Categoria: </span>
+                            <img class="product-category-icon" src="<?= $product->getCategory()->getIcon() ?>" alt="">
+                        </div>
+                        <div>Tipologia prodotto: <?= get_class($product)::getClassName() ?></div>
+                        <div class="card-text">Descrizione: <?= $product->getDescription() ?></div>
+                    </div>
+                </li>
+            <?php
+            }
+            ?>
+        </ul>
+    </div>
+
+    <!-- bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
