2a7b07364bd5ad13701bece43646b3b8604dc756
christianarca27 <christian.arca.mail@gmail.com>
2023-05-04 14:56:45 +0200

    feat: creata classe Food che estende la classe Product + relativo costruttore

---
+++ Models/Food.php
@@ -0,0 +1,17 @@
+<?php
+
+class Food extends Product
+{
+    private $quantity;
+    private $ingredients;
+    private $dosage;
+
+    function __construct(string $img, string $name, Category $category, float $price, string $description, string $quantity, array $ingredients, string $dosage)
+    {
+        parent::__construct($img, $name, $category, $price, $description);
+
+        $this->quantity = $quantity;
+        $this->ingredients = $ingredients;
+        $this->dosage = $dosage;
+    }
+}
